312e83a170b89067b3fb6f9d87ecce1e78d27c31
Erick Malta <erickmalta100@gmail.com>
2023-11-13 23:45:14 -0300

    feat(http): add routes

--- internal/http/server.go
+++ internal/http/server.go
@@ -19,10 +19,22 @@ func NewServer(port int) (*Server, error) {
 }
 
 func (s *Server) Start() error {
+	s.loadRoutes()
+
 	err := s.httpServer.Run()
 	if err != nil {
 		return fmt.Errorf("failed to start http server. err = %v", err)
 	}
 
 	return nil
+}
+
+func (s *Server) loadRoutes() {
+	s.httpServer.GET("/", s.healthCheck)
+}
+
+func (s *Server) healthCheck(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"status": "OK",
+	})
 }
