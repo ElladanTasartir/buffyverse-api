8dbdabb565228081cee1bfb4a0912e1a12a630b8
Erick Malta <erickmalta100@gmail.com>
2023-11-15 15:49:04 -0300

    feat(scrape): add scraping impl

--- internal/scraper/scraper.go
+++ internal/scraper/scraper.go
@@ -2,6 +2,7 @@ package scraper
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/ElladanTasartir/buffyverse-api/internal/entity"
 	"github.com/gocolly/colly"
@@ -9,7 +10,7 @@ import (
 
 type Scraper interface {
 	ScrapeCharacter(address string) (*entity.Character, error)
-	ScrapeCharacters() ([]*entity.Character, error)
+	ScrapeCharacters() ([]entity.Character, error)
 }
 
 type BuffyScraper struct {
@@ -50,19 +51,20 @@ func (b *BuffyScraper) ScrapeCharacter(address string) (*entity.Character, error
 	return &character, nil
 }
 
-func (b *BuffyScraper) ScrapeCharacters() ([]*entity.Character, error) {
+func (b *BuffyScraper) ScrapeCharacters() ([]entity.Character, error) {
 	searchPages := map[string]string{
-		"Scoobies": "Scooby_Gang",
-		"Angel":    "Angel_Investigations",
-		"Vamps":    "Category:Vampires",
-		"Slayers":  "Category:Slayers",
+		"Scoobies": "/wiki/Scooby_Gang",
+		"Angel":    "/wiki/Angel_Investigations",
+		"Vamps":    "/wiki/Category:Vampires",
+		"Slayers":  "/wiki/Category:Slayers",
 	}
 
-	pageMethods := map[string]func(address string) ([]*entity.Character, error){
+	pageMethods := map[string]func(address string) ([]entity.Character, error){
 		"Scoobies": b.scrapeScoobyMembers,
+		"Angel":    b.scrapeAngelMembers,
 	}
 
-	var characters []*entity.Character
+	var characters []entity.Character
 
 	for key, page := range searchPages {
 		endpoint := b.buildAddress(page)
@@ -82,10 +84,91 @@ func (b *BuffyScraper) ScrapeCharacters() ([]*entity.Character, error) {
 	return characters, nil
 }
 
-func (b *BuffyScraper) scrapeScoobyMembers(address string) ([]*entity.Character, error) {
-	return nil, nil
+func (b *BuffyScraper) scrapeScoobyMembers(address string) ([]entity.Character, error) {
+	var characters []entity.Character
+
+	b.collector.OnHTML("aside[role=region]", func(e *colly.HTMLElement) {
+		var membersHref []string
+
+		for _, node := range e.DOM.Find("div[data-source=Leader] > div > ul").Contents().Nodes {
+			for _, attr := range node.FirstChild.Attr {
+				if attr.Key == "href" {
+					membersHref = append(membersHref, attr.Val)
+					break
+				}
+			}
+		}
+
+		for _, node := range e.DOM.Find("div[data-source=Members] > div > ul").Contents().Nodes {
+			for _, attr := range node.FirstChild.Attr {
+				if attr.Key == "href" {
+					membersHref = append(membersHref, attr.Val)
+					break
+				}
+			}
+		}
+
+		for _, href := range membersHref {
+			character, err := b.ScrapeCharacter(b.buildAddress(href))
+			if err != nil {
+				log.Printf("Failed to scrape character: %s. err = %v", href, err)
+				continue
+			}
+
+			characters = append(characters, *character)
+		}
+	})
+
+	b.collector.OnRequest(func(f *colly.Request) {
+		log.Printf("scraping scoobies...")
+	})
+
+	err := b.collector.Visit(address)
+	if err != nil {
+		return characters, fmt.Errorf("failed to scrape scoobies. err = %v", err)
+	}
+
+	return characters, nil
+}
+
+func (b *BuffyScraper) scrapeAngelMembers(address string) ([]entity.Character, error) {
+	var characters []entity.Character
+
+	b.collector.OnHTML("aside[role=region]", func(e *colly.HTMLElement) {
+		var membersHref []string
+
+		for _, node := range e.DOM.Find("div[data-source=Members] > div > ul").Contents().Nodes {
+			for _, attr := range node.FirstChild.Attr {
+				if attr.Key == "href" {
+					membersHref = append(membersHref, attr.Val)
+					break
+				}
+			}
+		}
+
+		for _, href := range membersHref {
+			character, err := b.ScrapeCharacter(b.buildAddress(href))
+			if err != nil {
+				log.Printf("Failed to scrape character: %s. err = %v", href, err)
+				continue
+			}
+
+			characters = append(characters, *character)
+		}
+	})
+
+	b.collector.OnRequest(func(f *colly.Request) {
+		log.Printf("scraping angel investigations...")
+	})
+
+	err := b.collector.Visit(address)
+	if err != nil {
+		return characters, fmt.Errorf("faild to scrape angel investigations. err = %v", err)
+	}
+
+	return characters, nil
 }
 
 func (b *BuffyScraper) buildAddress(endpoint string) string {
-	return fmt.Sprintf("%s/%s", b.address, endpoint)
+	return fmt.Sprintf("%s%s", b.address, endpoint)
 }
